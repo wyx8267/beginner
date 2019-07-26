fde50d8621d1b32bd1aaf5361fd9a2f19735cc3d
wyx8267 <369264448@qq.com>
2019-07-26 18:28:19 +0800

    carousel demo2

---
+++ demo-jQuery-carousel/demo2/main.js
@@ -0,0 +1,44 @@
+let n
+let imageLength = $('.images>img').length
+init()
+
+setInterval(() => {
+    
+    makeLeave(getImage(n)).one('transitionend', (e)=>{
+        makeEnter($(e.currentTarget))
+    })
+    makeCurrent(getImage(n+1))
+    n++
+}, 3000);
+
+function getImage(n) {
+    return $(`.images>img:nth-child(${x(n, imageLength)})`)
+}
+
+function x(n, length) {
+    if (n > length) {
+        n = n % length
+        if(n === 0){
+            n = length
+        }
+    }
+    return n
+}
+
+function init() {
+    n =1
+    $(`.images>img:nth-child(${x(n, imageLength)})`).addClass('current').siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    $node.removeClass('enter leave').addClass('current')
+    return $node
+}
+function makeLeave($node) {
+    $node.removeClass('enter current').addClass('leave')
+    return $node
+}
+function makeEnter($node) {
+    $node.removeClass('current leave').addClass('enter')
+    return $node
+}
