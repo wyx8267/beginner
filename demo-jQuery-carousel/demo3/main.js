383393199f98b78d55ec0112f3dc7a302b63b106
wyx8267 <369264448@qq.com>
2019-07-29 16:14:11 +0800

    无缝轮播demo

---
+++ demo-jQuery-carousel/demo3/main.js
@@ -0,0 +1,68 @@
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({ transform: 'translateX(-400px)' })
+bindEvents()
+$('#next').on('click', function () {
+    goToSlide(current + 1)
+})
+$('#prev').on('click', function () {
+    goToSlide(current - 1)
+})
+
+let timer = setInterval(function () {
+    goToSlide(current + 1)
+}, 3000)
+
+$('.container').on('mouseenter', function () {
+    window.clearInterval(timer)
+})
+$('.container').on('mouseleave', function () {
+    timer = setInterval(function () {
+        goToSlide(current + 1)
+    }, 3000)
+})
+
+function bindEvents() {
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+}
+
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        //最后一张到第一张
+        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 400}px)` })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+            })
+    } else if (current === 0 && index === $buttons.length - 1) {
+        //第一张到最后一张
+        $slides.css({ transform: 'translateX(0)' })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+            })
+    } else {
+        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` })
+    }
+    current = index
+}
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
