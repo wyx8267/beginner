0f79cf80535617f8fce96003b7a4c5736120c850
wyx8267 <369264448@qq.com>
2019-08-01 15:59:06 +0800

    Promise

--- nodejs-cors-demo/main.js
+++ nodejs-cors-demo/main.js
@@ -4,29 +4,31 @@ window.jQuery = function (nodeOrSelector) {
     nodes.html = function () { }
     return nodes
 }
-window.jQuery.ajax = function ({ url, method, body, successFn, failFn, headers }) {
-    // let url = options.url
-    // let method = options.method
-    // let headers = options.headers
-    // let body = options.body
-    // let successFn = options.successFn
-    // let failFn = options.failFn
-    let request = new XMLHttpRequest()
-    request.open(method, url)
-    for (let koy in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                failFn.call(undefined, request)
+window.jQuery.ajax = function ({ url, method, body, headers }) {
+    return new Promise(function (resolve, reject) {
+        // let url = options.url
+        // let method = options.method
+        // let headers = options.headers
+        // let body = options.body
+        // let successFn = options.successFn
+        // let failFn = options.failFn
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        for (let koy in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key, value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })
 }
 window.$ = window.jQuery
 
@@ -41,5 +43,8 @@ myButton.addEventListener('click', (e) => {
         body: 'a=1&b=2',
         successFn: (x) => { console.log(x) },
         failFn: (x) => { console.log(x) }
-    })
+    }).then(
+        (text) => { console.log(text) },
+        (request) => { console.log(request) }
+    )
 })
