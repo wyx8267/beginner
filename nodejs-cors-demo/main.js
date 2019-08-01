c85f598cf9f2f61d41ee5e4da0338c6c71fc38d1
wyx8267 <369264448@qq.com>
2019-08-01 15:30:19 +0800

    实现jQuery AJAX

--- nodejs-cors-demo/main.js
+++ nodejs-cors-demo/main.js
@@ -1,17 +1,45 @@
-myButton.addEventListener('click', (e) => {
+window.jQuery = function (nodeOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () { }
+    nodes.html = function () { }
+    return nodes
+}
+window.jQuery.ajax = function ({ url, method, body, successFn, failFn, headers }) {
+    // let url = options.url
+    // let method = options.method
+    // let headers = options.headers
+    // let body = options.body
+    // let successFn = options.successFn
+    // let failFn = options.failFn
     let request = new XMLHttpRequest()
+    request.open(method, url)
+    for (let koy in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
             if (request.status >= 200 && request.status < 300) {
-                console.log('请求成功')
-                let string = request.responseText
-                let object=window.JSON.parse(string)
-                console.log(object)
-            } else if (request.status >= 400){
-                console.log('请求失败')
+                successFn.call(undefined, request.responseText)
+            } else if (request.status >= 400) {
+                failFn.call(undefined, request)
             }
         }
     }
-    request.open('GET', 'https://www.baidu.com')
-    request.send()
+    request.send(body)
+}
+window.$ = window.jQuery
+
+myButton.addEventListener('click', (e) => {
+    $.ajax({
+        url: '/xxx',
+        method: 'post',
+        header: {
+            'Content-Type': 'x-www-urlencoded',
+            'frank': '18'
+        },
+        body: 'a=1&b=2',
+        successFn: (x) => { console.log(x) },
+        failFn: (x) => { console.log(x) }
+    })
 })
